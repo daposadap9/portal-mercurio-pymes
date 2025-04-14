e6e5edffb58035a7cf0be70f999bdea6ad1e8e70
diegoposada1999 <diegoposada1999@gmail.com>
2025-04-14 12:26:28 -0500

    new changes

--- src/pages/paginas/servicios/mercurioDigitalizacion.js
+++ src/pages/paginas/servicios/mercurioDigitalizacion.js
@@ -375,12 +375,7 @@ const MercurioDigitalizacion = ({ disabledProvider }) => {
                 <p className="mb-2 text-center">No se ha seleccionado un plan</p>
               )}
               {user && (
-                <button
-                  className="w-full bg-teal-500 text-white px-4 py-2 rounded hover:bg-teal-600 transition-colors"
-                  onClick={handleEditOptionSummary}
-                >
-                  Editar Cotizador
-                </button>
+                <></>
               )}
               <div className="mt-4 flex justify-center">
                 <button
