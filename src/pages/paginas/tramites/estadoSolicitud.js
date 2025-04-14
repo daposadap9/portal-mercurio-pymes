a3af314d1006778a45f83c45e4c4ae614bd8e23c
diegoposada1999 <diegoposada1999@gmail.com>
2025-04-14 15:22:17 -0500

    new changes

--- src/pages/paginas/tramites/estadoSolicitud.js
+++ src/pages/paginas/tramites/estadoSolicitud.js
@@ -34,7 +34,7 @@ const EstadoSolicitud = () => {
 
   return (
     <div className="max-w-3xl mx-auto p-8">
-      <h1 className="text-3xl font-bold mb-6 text-center text-teal-700">
+      <h1 className="text-3xl font-bold mb-6 text-center text-teal-500">
         Consulta el Estado de tu Solicitud
       </h1>
 
