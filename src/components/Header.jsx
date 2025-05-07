e5bf36761d401fc8ef5714d5dbacfefed98f746e
diegoposada1999 <diegoposada1999@gmail.com>
2025-05-07 16:39:20 -0500

    new changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -308,19 +308,6 @@ const Header = ({ handleNavigation, loading }) => {
             </a>
           </Link>
 
-          {/* Opción: INICIO PRUEBA */}
-          <Link href="/inicioPrueba" legacyBehavior>
-            <a
-              onClick={handleLinkClick("/inicioPrueba")}
-              className={`${baseLinkClass} ${
-                isActive("/inicioPrueba")
-                  ? activeLinkClass
-                  : inactiveLinkClass
-              } text-sm`}
-            >
-              <FaDollarSign className="mr-1 text-base" /> INICIO PRUEBA
-            </a>
-          </Link>
 
           {/* Dropdown de TRÁMITES */}
           <div
@@ -469,20 +456,6 @@ const Header = ({ handleNavigation, loading }) => {
                 } text-sm text-teal-600`}
               >
                 <FaDollarSign className="mr-1 text-base" /> ¡COTIZA TU SERVICIO!
-              </a>
-            </Link>
-            <Link href="/inicioPrueba" legacyBehavior>
-              <a
-                onClick={handleLinkClick("/inicioPrueba", () =>
-                  setMobileMenuOpen(false)
-                )}
-                className={`${baseLinkClass2} ${
-                  isActive("/inicioPrueba")
-                    ? activeLinkClass
-                    : inactiveLinkClass
-                } text-sm text-teal-600`}
-              >
-                <FaDollarSign className="mr-1 text-base" /> INICIO PRUEBA
               </a>
             </Link>
             <button
